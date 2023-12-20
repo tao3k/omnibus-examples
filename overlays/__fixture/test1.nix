9a602a156f6cfd0dbcb8fbe5a4e312d7f5df949c
guangtao <gtrunsec@hardenedlinux.org>
2023-12-19 16:51:33 -0800

    first commit

---
+++ overlays/__fixture/test1.nix
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 The omnibus Authors
+#
+# SPDX-License-Identifier: MIT
+
+_:
+
+_final: _prev:
+{ }
---
+++ overlays/__fixture/test2.nix
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2023 The omnibus Authors
+#
+# SPDX-License-Identifier: MIT
+
+{ inputs }: _final: _prev: { }
