9c6f6d92deb3ab8d0bd257d73aef4a03376c0774
guangtao <gtrunsec@hardenedlinux.org>
2023-12-20 03:16:49 -0800

    Update quarto

--- jupyenv+quarto/nix/jupyenv/simple.nix
+++ jupyenv+quarto/nix/jupyenv/simple.nix
@@ -12,4 +12,17 @@
     env = inputs.self.packages.${pkgs.system}.poetry2nix-custom;
     # extraPackages = ps: [ps.scipy];
   };
+  kernel.julia.data-science = {
+    enable = true;
+    override = {
+      augmentedRegistry = pkgs.fetchFromGitHub {
+        owner = "CodeDownIO";
+        repo = "General";
+        rev = "840f93574326361e2614fc5a4c2413f07840215a";
+        sha256 = "sha256-UedaTpQwkuSZ/o4kLX/Jg8eDnL5IFI4XfYsJMRwBAKE=";
+      };
+      # Precompile = true;
+    };
+    extraJuliaPackages = [ "Plots" ];
+  };
 }
