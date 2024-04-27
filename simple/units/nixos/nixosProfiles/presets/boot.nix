a6cd8efa2eea0b07440ec281a9cce85f315b9129
guangtao <gtrunsec@hardenedlinux.org>
2024-04-26 20:05:30 -0700

    without scopeImport

--- simple/units/nixos/nixosProfiles/presets/boot.nix
+++ simple/units/nixos/nixosProfiles/presets/boot.nix
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2024 The omnibus Authors
 #
 # SPDX-License-Identifier: MIT
-
+{inputs, data}:
 {
   imports = [ inputs.self.nixosModules.boot ];
   boot.__profiles__.test = "nixosProfiles.boot";
